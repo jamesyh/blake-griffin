23ff6d5345cfa8574ad3ee59a5a7f0a645901275
jamesjamesjamez@gmail.com <james.elden.hill@gmail.com>
2017-04-26 15:42:10 -0600

    more stuff

--- binomial-threes.R
+++ binomial-threes.R
@@ -2,7 +2,7 @@
 
 library(R2jags)
 
-blake <- read.csv("C:/Users/James/Documents/My R Research/Blake G/blake griffin.txt")
+blake <- read.csv("C:/Users/James/Documents/My R Research/blake-griffen/blake-griffin/blake griffin.txt")
 
 threeMade <- blake$X3P[-8]
 threeAttempted <- blake$X3PA[-8]
@@ -63,7 +63,8 @@ mean(season7 > season1)
 
 
 
-plot(density(season1),col = 2,lwd = 3,ylim = c(0,9))
+plot(density(season1),col = 2,lwd = 3,ylim = c(0,9),
+     main = "Probability of Making a Three Pointer by Season")
 
 for(i in 2:7){
   
@@ -71,6 +72,18 @@ for(i in 2:7){
   
 }
 
+legend(.47,8.5,c("Season 1","Season 2","Season 3","Season 4","Season 5","Season 6","Season 7"),
+       lty=c(1,1),lwd=c(2.5,2.5),col=c(2,3,4,5,6,7,8))
 
 
+# prob <- c(mean(season1),mean(season2),mean(season3),mean(season4),mean(season5),mean(season6),mean(season7))
+# year <- 1:7
+# 
+# 
+# plot(log(prob) ~ year)
+# 
+# model <- lm(log(prob) ~ year)
+# summary(model)
+# 
+# abline(model)
 
--- poison-threes.R
+++ poison-threes.R
@@ -1,6 +1,7 @@
 # Has he started making more threes?
 
-blake <- read.csv("C:/Users/James/Documents/My R Research/Blake G/blake griffin game.txt")
+# Data scrapped from basketball reference
+blake <- read.csv("C:/Users/James/Documents/My R Research/blake-griffen/blake-griffin/blake griffin game.txt")
 
 threeMade <- as.numeric(as.character(blake$X3P))
 threeAttempted <- as.numeric(as.character(blake$X3PA))
@@ -22,13 +23,15 @@ year <- year[!is.na(threeMade)]
 threeAttempted <- threeAttempted[!is.na(threeMade)]
 threeMade <- threeMade[!is.na(threeMade)]
 
-threeAttempted <- threeMade
+#threeAttempted <- threeMade
+
+# For Made
 
 model_string <- "model{
   
   for(i in 1:466){
     
-    threeAttempted[i] ~ dpois(lambda[year[i]])
+    threeMade[i] ~ dpois(lambda[year[i]])
     
   }
   
@@ -41,6 +44,98 @@ model_string <- "model{
   alpha ~ dgamma(1,1)
   beta ~ dgamma(1,1)
   
+}"
+
+
+
+
+model <- jags.model(textConnection(model_string), 
+                    data = list(year = year,threeMade = threeMade))
+
+update(model, 10000, progress.bar="none"); # Burnin for 10000 samples
+
+samp <- coda.samples(model, 
+                     variable.names=c("lambda"), 
+                     n.iter=100000)
+
+
+draws <- as.matrix(samp)
+
+
+
+
+season1 <- draws[,1]
+season2 <- draws[,2]
+season3 <- draws[,3]
+season4 <- draws[,4]
+season5 <- draws[,5]
+season6 <- draws[,6]
+season7 <- draws[,7]
+
+
+
+mean(sample(season7) > sample(season6))
+mean(sample(season7) > sample(season5))
+mean(sample(season7) > sample(season4))
+mean(sample(season7) > sample(season3))
+mean(sample(season7) > sample(season2))
+mean(sample(season7) > sample(season1))
+
+
+
+plot(density(season1),col = 2,lwd = 3,ylim = c(0,20),xlim=c(0,1),
+     main = "Number of Threes Made by Season")
+
+for(i in 2:7){
+  
+  lines(density(draws[,i]),col = i + 1,lwd = 3)
+  
+}
+
+legend(.65,17.5,c("Season 1","Season 2","Season 3","Season 4","Season 5","Season 6","Season 7"),
+       lty=c(1,1),lwd=c(2.5,2.5),col=c(2,3,4,5,6,7,8))
+
+# prob <- c(mean(season1),mean(season2),mean(season3),mean(season4),mean(season5),mean(season6),mean(season7))
+# year <- 1:7
+# 
+# 
+# plot(prob ~ year)
+# 
+# model <- lm(prob ~ year)
+# summary(model)
+# 
+# abline(model)
+# 
+# tapply(threeAttempted,year,table)
+#
+# 
+# setwd("C:/Users/James/Documents/My R Research/Blake G/")
+# 
+# blake <- blake[!is.na(threeMade),]
+# 
+# write.csv(blake,"blake.csv")
+
+
+
+# For Attempted
+
+model_string <- "model{
+  
+for(i in 1:466){
+
+threeAttempted[i] ~ dpois(lambda[year[i]])
+
+}
+
+for(i in 1:7){
+
+lambda[i] ~ dgamma(alpha,beta)
+
+}
+
+alpha ~ dgamma(1,1)
+beta ~ dgamma(1,1)
+
 }"
 
 
@@ -80,7 +175,8 @@ mean(sample(season7) > sample(season1))
 
 
 
-plot(density(season1),col = 2,lwd = 3,ylim = c(0,15),xlim=c(0,1))
+plot(density(season1),col = 2,lwd = 3,ylim = c(0,8),xlim=c(0,2.5),
+     main = "Number of Threes Taken by Season")
 
 for(i in 2:7){
   
@@ -88,21 +184,6 @@ for(i in 2:7){
   
 }
 
+legend(1.7,7.5,c("Season 1","Season 2","Season 3","Season 4","Season 5","Season 6","Season 7"),
+       lty=c(1,1),lwd=c(2.5,2.5),col=c(2,3,4,5,6,7,8))
 
-
-for(i in 1:7){
-  hist(draws[,i])
-}
-
-
-
-
-
-tapply(threeAttempted,year,table)
-
-# 
-# setwd("C:/Users/James/Documents/My R Research/Blake G/")
-# 
-# blake <- blake[!is.na(threeMade),]
-# 
-# write.csv(blake,"blake.csv")
